1bde8d0698f2bb459b2851fd8e23ca5ff15f51aa
bulic <bulic@execom.eu>
2020-05-06 09:10:29 +0200

    bootstrap issue with nasing, make arr for some elements

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,8 +1,63 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-about",
-  templateUrl: "./about.component.html",
-  styleUrls: ["./about.component.scss"],
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  preventions = [
+    {
+      name: 'Wash your hands with soap and water.',
+      image: '../../../assets/5728191 - clean hands hygiene soap washing.png',
+    },
+    {
+      name: 'Avoid crowd and close contacts',
+      image:
+        '../../../assets/5728185 - avoid contact covid-19 crowd no people.png',
+    },
+    {
+      name: 'You should wear flu mask if going outside',
+      image:
+        '../../../assets/5728193 - covid-19 doctor flu mask protection wearing.png',
+    },
+    {
+      name: 'Stay home if you feel unwell.',
+      image:
+        '../../../assets/5728194 - covid-19 home hygiene prevent protection stay.png',
+    },
+  ];
+
+  symptoms = [
+    {
+      name: 'Headache',
+      image:
+        '../../../assets/5728192 - head headache migraine pain severe shock.png',
+    },
+    {
+      name: 'Difficulty breathing',
+      image:
+        '../../../assets/5728198 - bronchitis inflamation influenza lung pneumonia.png',
+    },
+    {
+      name: 'Chills',
+      image:
+        '../../../assets/5728182 - chill cold covid-19 fever flu shivering sick.png',
+    },
+    {
+      name: 'Coughing & sneezing',
+      image:
+        '../../../assets/5728201 - cough covid-19 flu sick sneezing sniffling wheezing.png',
+    },
+    {
+      name: 'Diarrhea',
+      image:
+        '../../../assets/5728186 - bowel constipation diarrhea irritable sick.png',
+    },
+    {
+      name: 'High body temperature',
+      image:
+        '../../../assets/5728190 - covid-19 fever high sick sweat temperature.png',
+    },
+  ];
+}
